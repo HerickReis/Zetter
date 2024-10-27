9ce3de7a01412bde65f3a1fa02e8fb16641b10ea
Herick <herickreis90.90@gmail.com>
2024-10-27 11:24:43 -0300

    Teste git ignore

--- a.py
+++ a.py
@@ -1,5 +1,14 @@
 from Downloader.main_downloader import run
 from utils.paths_manipulation import verificar_arquivo
 
+opcoes_funcionais = {1 : run, 2 : None, 3 : False}
+opcoes_printaveis = ["Downloader - Baixar videos da internet", None]
 
-run()
+switch = True
+
+while switch:
+    for pos, ops in enumerate(opcoes_printaveis):
+        print(f"{pos + 1} - {ops}")
+        
+    escolha = int(input("Escolha uma opção: "))
+    opcoes_funcionais[escolha]() if opcoes_funcionais[escolha] else print("\n",None, "\n")
